af42c945a91dbb53bf3e531c6a296475e43b52cb
Kumar <kumarv83@uscskumarv83m1.corp.emc.com>
2018-01-18 13:44:55 -0800

    create lib file, adjust main file

--- unregSdc.py
+++ unregSdc.py
@@ -3,50 +3,29 @@ import logging
 import os
 from requests.auth import HTTPBasicAuth
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import unregSdcLib
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-
+# ScaleIO object containing all variables
+# Upon instantiation, object will contain all Env Variables
+class ScaleIO(object):
     
-# instantiate Logger
-logging.basicConfig()
-alfred = logging.getLogger("Remove_SDC_Script")
+    def __init__(self):    
+        # instantiate Logger
+        logging.basicConfig()
+        self.alfred = logging.getLogger("Remove_SDC_Script")
 
-################################################
-# Check for environment variables
-# If appropriate environment variables do not exist
-# log error to loggin library and exit
-####################################################
+        self.server = None
+        self.user = None
+        self.password = None
+        self.removeIP = None
+        self.keyURL = None
+        self.sdcURL = None
+        unregSdcLib.checkSetEnvVars(self)
 
-if "HOST_IP" in os.environ:
-    server = os.getenv("HOST_IP")
-else:
-    alfred.error("Error reading Enviornment Variables. Please include the following: HOST_IP = <IP of ScaleIO Gateway>, USER = <username>, PASSWORD = <password>, REMOVE_SDC_IP = <IP address of SDC to remove>")
-    exit()
-
-if "USER" in os.environ:
-    user = os.getenv("USER")
-else:
-    alfred.error("Error reading Enviornment Variables. Please include the following: HOST_IP = <IP of ScaleIO Gateway>, USER = <username>, PASSWORD = <password>, REMOVE_SDC_IP = <IP address of SDC to remove>")
-    exit()
-
-if "PASSWORD" in os.environ:    
-    password = os.getenv("PASSWORD")
-else:
-    alfred.error("Error reading Enviornment Variables. Please include the following: HOST_IP = <IP of ScaleIO Gateway>, USER = <username>, PASSWORD = <password>, REMOVE_SDC_IP = <IP address of SDC to remove>")
-    exit()
-    
-if "REMOVE_SDC_IP" in os.environ:
-    removeIP = os.getenv("REMOVE_SDC_IP")
-else:
-    alfred.error("Error reading Enviornment Variables. Please include the following: HOST_IP = <IP of ScaleIO Gateway>, USER = <username>, PASSWORD = <password>, REMOVE_SDC_IP = <IP address of SDC to remove>")
-    exit()
-    
-#####################
-# build URLs
-######################
-keyURL = server + "/api/login"
-sdcURL = server + "/api/types/Sdc/instances"
+# create ScaleIO object
+sio = ScaleIO()
 
 ####################
 # start Http Session
@@ -56,17 +35,9 @@ _session = requests.Session()
 ##############################################
 # make key call and receive authorization token
 ################################################
-token = _session.get(keyURL, auth=HTTPBasicAuth(user, password), verify=False)
-
-################################
-# ensure response code is 200
-# exit if code not 200
-# report error via logging mechanism
-######################################
-if  token.status_code != 200:
-    alfred.error("The process exited with a {} status".format(token.status_code))
-    alfred.error("The error message states: {}".format(token.content))
-    exit()
+token = _session.get(sio.keyURL, auth=HTTPBasicAuth(sio.user, sio.password), verify=False)
+# Check for http Errors
+unregSdcLib.httpErrorCheck(sio, token)
 
 ####################################
 # Add authorization token to session
@@ -76,62 +47,32 @@ _session.auth = HTTPBasicAuth('', token.json())
 ####################
 # get list of SDCs
 #####################
-res = _session.get(sdcURL)
-
-########################
-# Status code checking
-#######################
-if  res.status_code != 200:
-    alfred.error("The process exited with a {} status".format(res.status_code))
-    alfred.error("The error message states: {}".format(res.content))
-    exit()
+res = _session.get(sio.sdcURL)
+unregSdcLib.httpErrorCheck(sio, res)
 
 ##########################
 # Print length of SDC List
 ###########################
-alfred.info("There are {} SDCs registered before execution".format(len(res.content)))
-
+sio.alfred.info("There are {} SDCs registered before execution".format(len(res.content)))
 
 #################################################
 # find id of SDC by looping through list of SDCs
 # returned from ScaleIO
 #################################################
-sdcId = None
-
-for i in res.json():
-    if i['sdcIp'] == removeIP:
-        sdcId = i["id"]
-        # ensure SDC is not connected to MDM
-        if i["mdmConnectionState"] == "Connected":
-            alfred.error("The SDC with IP {} is connected to the MDM. Cannot unregister.".format(removeIP))
-            exit()
-        # exit loop
-        break
-
-##########################################################
-# Exit script if we do not find matching SDC to IP address
-# found in environment variables
-###########################################################
-if sdcId == None:
-    alfred.error("Can't find an SDC with IP {}".format(removeIP))
-    exit()
+sdcId = unregSdcLib.checkSdcId(sio, res)
 
 ##################
 # unregister SDC
 ###################
-sdcUnregisterSdcURL = server + "/api/instances/Sdc::" + sdcId + "/action/removeSdc"
+sdcUnregisterSdcURL = sio.server + "/api/instances/Sdc::" + sdcId + "/action/removeSdc"
 res = _session.post(sdcUnregisterSdcURL)
-
-if  res.status_code != 200:
-    alfred.error("The process exited with a {} status".format(res.status_code))
-    alfred.error("The error message states: {}".format(res.content))
-    exit()
+unregSdcLib.httpErrorCheck(sio, res)
 
 ##############################################
 # Log final HTTP status with logging mechanism
 ################################################
-alfred.info("The process exited with a {} status".format(res.status_code))
-alfred.info("The error message states: {}".format(res.content))
+sio.alfred.info("The process exited with a {} status".format(res.status_code))
+sio.alfred.info("The error message states: {}".format(res.content))
 
 
 
---
+++ unregSdcLib.py
@@ -0,0 +1,55 @@
+import os
+
+################################################
+# Check for environment variables
+# If appropriate environment variables do not exist
+# log error to loggin library and exit
+####################################################
+def checkSetEnvVars (sio):
+    if "HOST_IP" in os.environ:
+        sio.server = os.getenv("HOST_IP")
+    else:
+        sio.alfred.error("Error reading Enviornment Variables. Please include the following: HOST_IP = <IP of ScaleIO Gateway>, USER = <username>, PASSWORD = <password>, REMOVE_SDC_IP = <IP address of SDC to remove>")
+        exit()
+
+    if "USER" in os.environ:
+        sio.user = os.getenv("USER")
+    else:
+        sio.alfred.error("Error reading Enviornment Variables. Please include the following: HOST_IP = <IP of ScaleIO Gateway>, USER = <username>, PASSWORD = <password>, REMOVE_SDC_IP = <IP address of SDC to remove>")
+        exit()
+
+    if "PASSWORD" in os.environ:    
+        sio.password = os.getenv("PASSWORD")
+    else:
+        sio.alfred.error("Error reading Enviornment Variables. Please include the following: HOST_IP = <IP of ScaleIO Gateway>, USER = <username>, PASSWORD = <password>, REMOVE_SDC_IP = <IP address of SDC to remove>")
+        exit()
+
+    if "REMOVE_SDC_IP" in os.environ:
+        sio.removeIP = os.getenv("REMOVE_SDC_IP")
+    else:
+        sio.alfred.error("Error reading Enviornment Variables. Please include the following: HOST_IP = <IP of ScaleIO Gateway>, USER = <username>, PASSWORD = <password>, REMOVE_SDC_IP = <IP address of SDC to remove>")
+        exit()
+    
+    sio.keyURL = sio.server + "/api/login"
+    sio.sdcURL = sio.server + "/api/types/Sdc/instances"
+        
+        
+def checkSdcId (sio, res):        
+    for i in res.json():
+        if i['sdcIp'] == sio.removeIP:
+            # ensure SDC is not connected to MDM
+            if i["mdmConnectionState"] == "Connected":
+                sio.alfred.error("The SDC with IP {} is connected to the MDM. Cannot unregister.".format(sio.removeIP))
+                exit()
+            else:
+                return i["id"]
+        # if function has not returned or exited, then we can't find SDC.
+        # report error and exit.
+        sio.alfred.error("Can't find an SDC with IP {}".format(sio.removeIP))
+        exit()
+        
+def httpErrorCheck (sio, res):
+    if  res.status_code != 200:
+        sio.alfred.error("The process exited with a {} status".format(res.status_code))
+        sio.alfred.error("The error message states: {}".format(res.content))
+        exit()
